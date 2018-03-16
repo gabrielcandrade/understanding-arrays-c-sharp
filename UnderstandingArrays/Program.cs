66bc4cb4b2a259865b7c3f9516e302a7ce7c8f79
Gabriel Andrade <gabrielcandrade01@gmail.com>
2018-03-16 11:35:57 -0300

    Subindo projeto

---
+++ UnderstandingArrays/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //numbers[5] = 42; // Cause it only fits 5 elements.
+
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 }; // This way, it fits how many i like to
+
+            //string[] names = new string[] { "Gabriel", "Larissa", "Michael", "Pedro" };
+
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            */ // Here how to across all the array printing each.
+
+            /*
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */ // Here how to across all the array using the foreach.
+
+            string zig = "You can get what you want out of life" +
+                " if you help enough other people get what they want. ";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
